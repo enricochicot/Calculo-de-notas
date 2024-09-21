848a19138507841180f845c16ac07993ea4f8ea3
Enrico Leite <enricoleite7@gmail.com>
2024-09-20 21:21:23 -0300

    Add files via upload

---
+++ exercicio 1.cpp
@@ -0,0 +1,20 @@
+
+#include <iostream>
+
+using namespace std;
+int main()
+{
+   double nota1;
+   cout <<"Insira a primeira nota do aluno: ";
+   cin >> nota1;
+   
+   double nota2;
+   cout <<"Insira a segunda nota do aluno: ";
+   cin>>nota2;
+   
+   double media;
+   media = (nota1+nota2)/2;
+   
+   
+   cout << "A média do aluno foi: " << media;
+}
